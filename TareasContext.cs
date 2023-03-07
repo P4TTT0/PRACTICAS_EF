3e7840a427a3b2b7306461460adc744a348ebd27
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-06 22:42:47 -0300

    Proyecto EF - Creacion de MODELOS

---
+++ TareasContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURSO_FUNDAMENTOS_EF
+{
+    public class TareasContext
+    {
+        
+    }
+}
