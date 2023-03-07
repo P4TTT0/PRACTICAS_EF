83a952d5cfc2f0e3708b090d615d461e2e4eb255
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-07 19:30:09 -0300

    Proyecto EF - Conexion a BD SQL Server

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB")); //Metodo que nos permite agregar un contexto de base de datos en memoria.
-
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB")); //Metodo que nos permite agregar un contexto de base de datos en memoria.
+builder.Services.AddSqlServer<TareasContext>(@"Data Source = DESKTOP-RNP88KR\MSSQLSERVERDEV; Initial Catalog = TareasDb; user id=sa; password = sa; Trusted_Connection=True; Integrated Security=True;Trust Server Certificate=true");
+//Configura una instancia de SQL Server mediante un PATH (CADENA DE CONEXION)
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
