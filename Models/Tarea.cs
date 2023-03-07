499705b63543ed8c14d2143d343720dfe96ccb1d
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-06 22:59:05 -0300

    Proyecto EF - Utilizacion de DataAnnotations

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,25 @@ namespace CURSO_FUNDAMENTOS_EF.Models
 {
     public class Tarea
     {
-        
+        [Key] //DataAnnotations -> Forzamos a utilizar esta propiedad como PK.
+        public Guid TareaId {get; set;}
+        [ForeignKey("CategoriaId")] //DataAnnotations -> Forzamos a utilizar esta propiedad como FK bajo el nombre 'CategoriaId'
+        public Guid CategoriaId {get; set;}
+        [Required] //DataAnnotations -> Forzamos a que esta propiedad sea requeridad al momento en el que nosotros intentemos insertar un nuevo registro dentro de la tabla categorias.
+        [MaxLength(200)] //DattaAnnotations -> Limitamos la cantidad de caracteres 
+        public string Titulo {get; set;}
+        public string Descripcion {get; set;}
+        public Prioridad PrioridadTarea {get; set;}
+        public DateTime FechaCreacion {get; set;}
+
+        public virtual Categoria Categoria {get; set;}
     }
+
+    public enum Prioridad
+    {
+        Baja,
+        Media,
+        Alta
+    }
+    
 }
