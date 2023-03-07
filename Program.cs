1f52216e5b6bb7b88ebba077485241f70c5c5247
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-07 19:07:22 -0300

    Proyecto EF - Base de datos en MEMORIA V2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB")); //Metodo que nos permite agregar un contexto de base de datos en memoria.
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -13,6 +14,6 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
-});
+}); // Creamos un ENDPOINT que devolvera un mensaje que indica si la base de datos utilizada por el contexto se ha creado correctamente.
 
 app.Run();
