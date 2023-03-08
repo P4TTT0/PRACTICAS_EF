fe086e686981da2ef35d9524d6ec150671fc8537
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-08 20:34:26 -0300

    Proyecto EF - Obtencion de datos con EF

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CURSO_FUNDAMENTOS_EF.Models
@@ -27,6 +28,7 @@ namespace CURSO_FUNDAMENTOS_EF.Models
         public string Nombre {get; set;}
         public string Descripcion {get; set;}
         public int Peso {get; set;}
+        [JsonIgnore]
         public virtual ICollection<Tarea> Tareas {get; set;}
 
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using CURSO_FUNDAMENTOS_EF;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CURSO_FUNDAMENTOS_EF.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,5 +17,10 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
     dbContext.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
 }); // Creamos un ENDPOINT que devolvera un mensaje que indica si la base de datos utilizada por el contexto se ha creado correctamente.
+
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == Prioridad.Alta));
+});
 
 app.Run();
