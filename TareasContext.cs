6aface9aa4497b3c09ac450d98ed69976c53ada0
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-07 21:27:31 -0300

    Proyecto EF - Utilizacion de FLUENT API

--- TareasContext.cs
+++ TareasContext.cs
@@ -12,5 +12,29 @@ namespace CURSO_FUNDAMENTOS_EF
         public DbSet<Categoria> Categorias {get; set;}
         public DbSet<Tarea> Tareas {get; set;}
         public TareasContext(DbContextOptions<TareasContext> options) : base (options) {  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(categoria =>
+            {
+                categoria.ToTable("Categoria");
+                categoria.HasKey(p => p.CategoriaId);
+                categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
+                categoria.Property(p => p.Descripcion);
+            });    
+
+            modelBuilder.Entity<Tarea>(tarea =>
+            {
+                tarea.ToTable("Tarea"); //Indicamos que será una tabla y el nombre de la misma
+                tarea.HasKey(t => t.TareaId); //Indicamos que la talba posee PK
+                tarea.HasOne(t => t.Categoria).WithMany(t => t.Tareas).HasForeignKey(t => t.CategoriaId); //Indicamos la FK de conexion entre Tarea y Categoria.
+                tarea.Property(t => t.Titulo).IsRequired().HasMaxLength(200); //Indicamos que la propiedad TITULO es NOT NULL y tiene un LEN de 200 caracteres.
+                tarea.Property(t => t.Descripcion);
+                tarea.Property(t => t.PrioridadTarea);
+                tarea.Property(t => t.FechaCreacion);
+
+            });
+        }
+
     }
 }
