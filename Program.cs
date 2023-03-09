3f15f466511f83a771ea8a7d89a3fa9974a0ad98
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-08 21:59:49 -0300

    GUARDAR y ACTUALIZAR datos con EF

--- Program.cs
+++ Program.cs
@@ -20,7 +20,42 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
-    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == Prioridad.Alta));
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    Tarea tareaActual = dbContext.Tareas.Find(id);
+    IResult resultado;
+
+    if(tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+        resultado = Results.Ok();
+    }
+    else
+    {
+        resultado = Results.NotFound();
+    }
+
+    return resultado;
+});
+
 
 app.Run();
